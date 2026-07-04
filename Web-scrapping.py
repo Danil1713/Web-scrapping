23399920fb98f1f853af142fabb9d82cb5569ab0
Danil <danilderbassov10313@gmail.com>
2026-07-04 19:14:52 +0300

    revision

--- Web-scrapping.py
+++ Web-scrapping.py
@@ -7,10 +7,8 @@ URL = "https://habr.com/ru/articles/"
 
 response = requests.get(URL)
 soup = BeautifulSoup(response.content, "lxml")
-print(f"📊 Статус ответа: {response.status_code}")
 
 articles = soup.find_all("div", class_="article-snippet" )
-print(f"📝 Найдено статей на странице: {len(articles)}")
 
 for article in articles:
     title_tag = article.find("h2", class_="tm-title tm-title_h2")
@@ -25,8 +23,15 @@ for article in articles:
     preview_tag = article.find('div', class_='article-formatted-body article-formatted-body article-formatted-body_version-2')
     preview_text = preview_tag.text.strip() if preview_tag else ""
 
-    full_text = (preview_text + ' ' + title).lower()
+    hubs_tag = article.find('div', class_='tm-publication-hubs')
+    hubs_text = ""
+    if hubs_tag:
+        hub_links = hubs_tag.find_all('span', class_='tm-publication-hub__link-container')
+        hubs_text = ' '.join([hub.text.strip() for hub in hub_links])
+
+    full_text = (preview_text + ' ' + title + ' ' + hubs_text).lower()
     for keyword in KEYWORDS:
         pattern = re.compile(keyword, re.IGNORECASE)
         if pattern.search(full_text):
-            print(f"{date}-{title}-{full_text}")
+            print(f"{date}-{title}-{link}")
+            break
